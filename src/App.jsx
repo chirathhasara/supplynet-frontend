f103eeb5de45d70b4c152ce3bc6dfa221fa6309c
chirath <chirathhasara2001@gmal.com>
2025-09-09 21:30:06 +0530

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+    
+    </BrowserRouter>
+  )
+ 
+}
+
+export default App
